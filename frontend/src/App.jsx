80774601573c14a6ffa7eee61268b9104b9218fe
= <=>
2025-01-14 17:03:53 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen bg-black'>
+      <h1 className='text-4xl font-bold text-white'>Hello, This is React and Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
